3879c249844d920004fa342fd5fe19b1a47665dd
MiqueasAguirreANX <70060960+MiqueasAguirreANX@users.noreply.github.com>
2021-05-10 12:57:07 -0300

    gymnasta front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { ProfileComponent } from './profile/profile.component';
+import { VideosComponent } from './videos/videos.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'videos', component: VideosComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { VideosComponent } from './videos/videos.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
+import { VideoComponent } from './video/video.component';
+import { PanelComponent } from './panel/panel.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProfileComponent,
+    VideosComponent,
+    SubscriptionComponent,
+    VideoComponent,
+    PanelComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
